93ed540a3525ed3e3d61fd1c66eb5a7c7a9ddaa2
Justin Chen <24justinch@students.harker.org>
2020-10-15 16:46:14 -0700

    partially set up talons

--- src/main/java/frc/robot/subsystem/Drivetrain2.java
+++ src/main/java/frc/robot/subsystem/Drivetrain2.java
@@ -1,17 +1,21 @@
 package frc.robot.subsystem;
 
-import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import harkerrobolib.wrappers.HSTalon;
+import frc.robot.RobotMap;
 
-public class Drivetrain2 {
+public class Drivetrain2 extends SubsystemBase{
     private static Drivetrain2 drivetrain;
     private HSTalon rightMaster;
     private HSTalon leftMaster;
     private HSTalon rightHelper;
     private HSTalon leftHelper;
     private Drivetrain2(){
-
-
+        rightMaster=new HSTalon(RobotMap.RIGHT_MASTER_ID);
+        rightHelper=new HSTalon(RobotMap.RIGHT_FOLLOWER_ID);
+        leftMaster=new HSTalon(RobotMap.LEFT_MASTER_ID);
+        leftHelper=new HSTalon(RobotMap.RIGHT_FOLLOWER_ID);
+        initMotors();
     }
     public static Drivetrain2 getInstance(){
         if (drivetrain==null){
@@ -20,6 +24,11 @@ public class Drivetrain2 {
         return drivetrain;
 
     }
+
+    public void talonInit(){
+        
+    }
+    //wait after this lets commitkk
     public void initMotors(){
         rightHelper.configFactoryDefault();
         rightMaster.configFactoryDefault();
@@ -27,4 +36,6 @@ public class Drivetrain2 {
         leftMaster.configFactoryDefault();
         
     }
+
+
 }
