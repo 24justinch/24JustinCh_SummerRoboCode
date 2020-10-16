a1b40424efd7f239bc25e83b92c6c04f63325616
Justin Chen <24justinch@students.harker.org>
2020-10-15 17:22:09 -0700

    finished setPercentOutput (hopefully) and set up sensorphase

--- src/main/java/frc/robot/subsystem/Drivetrain2.java
+++ src/main/java/frc/robot/subsystem/Drivetrain2.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystem;
 
+import java.util.ResourceBundle.Control;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import harkerrobolib.wrappers.HSTalon;
 import frc.robot.RobotMap;
@@ -10,12 +14,16 @@ public class Drivetrain2 extends SubsystemBase{
     private HSTalon leftMaster;
     private HSTalon rightHelper;
     private HSTalon leftHelper;
+    private boolean leftInverted = false;
+    private boolean rightInverted = false;
+    private boolean leftSensorPhase=false;
+    private boolean rightSensorphase = false;
+
     private Drivetrain2(){
         rightMaster=new HSTalon(RobotMap.RIGHT_MASTER_ID);
         rightHelper=new HSTalon(RobotMap.RIGHT_FOLLOWER_ID);
         leftMaster=new HSTalon(RobotMap.LEFT_MASTER_ID);
         leftHelper=new HSTalon(RobotMap.RIGHT_FOLLOWER_ID);
-        initMotors();
     }
     public static Drivetrain2 getInstance(){
         if (drivetrain==null){
@@ -26,16 +34,26 @@ public class Drivetrain2 extends SubsystemBase{
     }
 
     public void talonInit(){
-        
-    }
-    //wait after this lets commitkk
-    public void initMotors(){
         rightHelper.configFactoryDefault();
         rightMaster.configFactoryDefault();
         leftHelper.configFactoryDefault();
         leftMaster.configFactoryDefault();
+        leftHelper.follow(leftMaster);
+        rightHelper.follow(rightMaster);
+        rightMaster.setInverted(rightInverted);
+        leftMaster.setInverted(leftInverted);
+        leftMaster.setSensorPhase(leftSensorPhase);
+        rightMaster.setSensorPhase(rightSensorphase);
         
     }
+    public void setPercentOutput(double value,double turn){
+            leftMaster.set(ControlMode.PercentOutput, value+turn);
+            rightMaster.set(ControlMode.PercentOutput, value-turn);
+
+        }
+    }
+    
+
 
 
 }
